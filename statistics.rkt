b29afc01de9981ffe6a49c1ee8430173686017f6
Siyuan Chen <chansey97@outlook.com>
2025-03-09 04:47:01 +0800

    Update statistics.rkt and conclusion.md.

--- statistics.rkt
+++ statistics.rkt
@@ -409,7 +409,7 @@
          (individual-pulls-per-5-stars-max (exact->inexact (apply max individual-pulls-per-5-stars-samples)))
          (individual-pulls-per-5-stars-min (exact->inexact (apply min individual-pulls-per-5-stars-samples)))
          (individual-pulls-per-5-stars-stddev (exact->inexact (stddev individual-pulls-per-5-stars-samples))))
-    (printf "==== 统计 ~a 当获取惊喜奖励时，每~a抽数 （样本量：~a） ====\n" pool-name "5星英雄" sample-size)
+    (printf "==== 统计 ~a 当抽出限定时，每~a抽数 （样本量：~a） ====\n" pool-name "5星英雄" sample-size)
     (printf "平均抽数（宏观指标）: ~a\n" global-pulls-per-5-stars)
     (printf "平均抽数: ~a\n" individual-pulls-per-5-stars-average)
     (printf "中位数: ~a\n" individual-pulls-per-5-stars-median)
@@ -426,11 +426,9 @@
   (printf "run-statistics-7:\n")
   (define limited-pool limited-spirits-pool)
   (define limited-pool-name (get-field name limited-pool))
-  (define limited-pool-rarities (get-field base-rarities limited-pool))
   (define limited-pool-up-hero (get-field up-hero limited-pool))
   (define adjoint-pool (if adjoint-is-crazy crazy-spirits-pool normal-spirits-pool))
   (define adjoint-pool-name (get-field name adjoint-pool))
-  (define adjoint-pool-rarities (get-field base-rarities adjoint-pool))
   (define shared-pity-system (get-field pity-system limited-pool))
 
   (define (dual-lego-optimization-strategy)
@@ -440,20 +438,24 @@
     
     (let loop ((pull-count 0)
                (5-stars-count 0)
-               (status 'start-in-limited))
+               (status 'start-in-limited)
+               (status-chains '((start-in-limited 0))))
       (match status
         ['start-in-limited
-         (if (<= pull-count start-in-limited-pulls)
+         (if (< pull-count start-in-limited-pulls)
              (let* ((cards (send limited-pool pull))
                     (card (first cards))
                     (hero (card-hero card))
                     (rarity (card-rarity card)))
                (if (= (rarity-stars rarity) 5)
                    (if (string=? hero limited-pool-up-hero)
-                       (list (add1 pull-count) (add1 5-stars-count))
-                       (loop (add1 pull-count) (add1 5-stars-count) 'continue-in-limited))
-                   (loop (add1 pull-count) 5-stars-count 'start-in-limited)))
-             (loop pull-count 5-stars-count 'pull-in-adjoint))]
+                       (list (add1 pull-count) (add1 5-stars-count)
+                             (append status-chains (list (list status (add1 pull-count)))))
+                       (loop (add1 pull-count) (add1 5-stars-count) 'continue-in-limited
+                             (append status-chains (list (list 'continue-in-limited (add1 pull-count))))))
+                   (loop (add1 pull-count) 5-stars-count status status-chains)))
+             (loop pull-count 5-stars-count 'pull-in-adjoint
+                   (append status-chains (list (list 'pull-in-adjoint pull-count)))))]
         ['continue-in-limited
          (let* ((cards (send limited-pool pull))
                 (card (first cards))
@@ -461,25 +463,33 @@
                 (rarity (card-rarity card)))
            (if (= (rarity-stars rarity) 5)
                (if (string=? hero limited-pool-up-hero)
-                   (list (add1 pull-count) (add1 5-stars-count))
-                   (loop (add1 pull-count) (add1 5-stars-count) 'continue-in-limited))
-               (loop (add1 pull-count) 5-stars-count 'continue-in-limited)))]
+                   (list (add1 pull-count) (add1 5-stars-count)
+                         (append status-chains (list (list status (add1 pull-count)))))
+                   (loop (add1 pull-count) (add1 5-stars-count) status status-chains))
+               (loop (add1 pull-count) 5-stars-count status status-chains)))]
         ['pull-in-adjoint
          (let* ((cards (send adjoint-pool pull))
                 (card (first cards))
                 (rarity (card-rarity card)))
            (if (= (rarity-stars rarity) 5)
-               (loop (add1 pull-count) (add1 5-stars-count) 'continue-in-limited)
-               (loop (add1 pull-count) 5-stars-count 'pull-in-adjoint)))]
+               (loop (add1 pull-count) (add1 5-stars-count) 'continue-in-limited
+                     (append status-chains (list (list 'continue-in-limited (add1 pull-count)))))
+               (loop (add1 pull-count) 5-stars-count status status-chains)))]
         )))
   (let* ((samples (for/list ([i (in-range sample-size)]) (dual-lego-optimization-strategy)))
          (lst-of-pull-count (map first samples))
          (lst-of-5-stars-count (map second samples))
+         (lst-of-status-chains (map third samples))
          (global-pulls-per-5-stars (exact->inexact (/ (apply + lst-of-pull-count) (apply + lst-of-5-stars-count))))
          (individual-pulls-per-5-stars-samples (map / lst-of-pull-count lst-of-5-stars-count))
          (individual-pulls-per-5-stars-average (exact->inexact (mean individual-pulls-per-5-stars-samples)))
          (individual-pulls-per-5-stars-median (exact->inexact (median < individual-pulls-per-5-stars-samples)))
          (individual-pulls-per-5-stars-max (exact->inexact (apply max individual-pulls-per-5-stars-samples)))
+         ;; (individual-pulls-per-5-stars-max-detail
+         ;;  (let ((max-index (index-of individual-pulls-per-5-stars-samples (exact-round individual-pulls-per-5-stars-max))))
+         ;;    (list (list-ref lst-of-pull-count max-index)
+         ;;          (list-ref lst-of-5-stars-count max-index)
+         ;;          (list-ref lst-of-status-chains max-index))))
          (individual-pulls-per-5-stars-min (exact->inexact (apply min individual-pulls-per-5-stars-samples)))
          (individual-pulls-per-5-stars-stddev (exact->inexact (stddev individual-pulls-per-5-stars-samples))))
     (printf "==== 统计 ~a + ~a 采用卡双金策略时，每~a抽数 （样本量：~a） ====\n"
@@ -500,8 +510,14 @@
     (printf "平均抽数: ~a\n" individual-pulls-per-5-stars-average)
     (printf "中位数: ~a\n" individual-pulls-per-5-stars-median)
     (printf "最大值: ~a\n" individual-pulls-per-5-stars-max)
+    ;; (printf "最大值的策略细节：~a\n" individual-pulls-per-5-stars-max-detail)
     (printf "最小值: ~a\n" individual-pulls-per-5-stars-min)
     (printf "标准差: ~a\n" individual-pulls-per-5-stars-stddev)
     (printf "\n")
     ))
 
+
+;; (for ([i (in-range 10 200 10)])
+;;   (run-statistics-7 10000 0 i))
+
+;; (run-statistics-7 10000 0 190)
